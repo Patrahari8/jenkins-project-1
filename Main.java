0e4ebb3697c042f9f002d5c3e5b6492a564090db
Hari Patra <harihar.patra.29.2@mountblue.tech>
2024-10-01 16:24:47 +0530

    Added New File

--- Main.java
+++ Main.java
@@ -4,5 +4,7 @@ class Main{
     public static void main(String[] args) {
         System.out.println("This Is A Java Program");
         System.out.println("The Time Is "+ new Date());
+        System.out.println("Added New File");
+        System.out.println("Using Jenkins to Build");
     }
 }
