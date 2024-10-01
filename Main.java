105340fd3bfec8cc52eebb9478d78f87b800de0f
Hari Patra <harihar.patra.29.2@mountblue.tech>
2024-10-01 16:16:37 +0530

    Intial Commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+class Main{
+    public static void main(String[] args) {
+        System.out.println("This Is A Java Program");
+        System.out.println("The Time Is "+ new Date());
+    }
+}
